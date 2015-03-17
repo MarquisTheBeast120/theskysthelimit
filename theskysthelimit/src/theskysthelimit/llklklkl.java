4ee1c8bce7e6e0dd1d662df9cd976eff11f81d56
mburgos5617 <mburgos5617@001LAB102010535.neisd.ad>
2015-03-17 15:19:27 -0500

    chicken

--- theskysthelimit/src/theskysthelimit/llklklkl.java
+++ theskysthelimit/src/theskysthelimit/llklklkl.java
@@ -7,19 +7,59 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import theskysthelimit.ez.DrawPanel;
+
 
 public class llklklkl {
 	public llklklkl(){
-		frame();	
-		}
+		bveudf();
+			
+		
+	}
+	
+		
+	
+	
+	public void bveudf(){
+		
+		  JFrame e = new JFrame();
+		  e.setSize(600,600);
+		  e.setVisible(true);
+		  e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  
+		  JPanel v = new JPanel();
+		   JLabel m = new JLabel("Click here to enter the zoo"); 
+		  
+		  JButton B0 = new JButton("enter");
+		  B0.addActionListener(new ActionListener(){
+			  @Override
+			  public void actionPerformed(ActionEvent q)
+			  {
+				  frame();
+			  }
+			 
+  });  
+		  e.add(v);
+		  v.add(m);
+		  v.add(B0);
+	}
+	
 	
 	public void frame(){
-		
+		//JFrame
+		  
 		  JFrame w = new JFrame();
 		  w.setSize(400,400);
 		  w.setVisible(true);
 		  w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  //JPanel
+		  JPanel v = new JPanel();
 		  JPanel p = new JPanel();
+		  	  
+		  //all buttons	
+		 
 		  JButton b1 = new JButton("pig"); 
 		 JButton b2 = new JButton("cat");
 		 JButton b3 = new JButton("snake");
@@ -30,11 +70,16 @@ public class llklklkl {
 		 JButton b8 = new JButton("shark");
 		 JButton b9= new JButton("zebra");
 		 
+		//button's actions 
+		 
+  
 		  b1.addActionListener(new ActionListener(){
 			  @Override
 			  public void actionPerformed(ActionEvent e)
 			  {
-				JOptionPane.showMessageDialog(null, "pig");
+				  ez X = new ez();
+				  DrawPanel drawPanel = X.new DrawPanel();
+				  
 			  }
 			 
   });  
@@ -96,7 +141,8 @@ public class llklklkl {
 				 });
 			 
 			 
-		  
+	     
+	   
 		  w.add(p);
 		  p.add(b1);
 		  p.add(b2);
@@ -112,6 +158,9 @@ public class llklklkl {
 		  
 		  
 		  }
+	
+	
+	
 	public static void main(String[] args)
 	{
 	new llklklkl();
